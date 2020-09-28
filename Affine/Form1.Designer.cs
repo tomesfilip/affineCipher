66c1a7179d13fb899cf496941ca1a75176595318
tomesfilip <filiptomes@protonmail.com>
2020-09-28 13:03:05 +0200

    main functionality done

---
+++ Affine/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace Affine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.encryptBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxEncrypt = new System.Windows.Forms.TextBox();
+            this.textBoxDecrypt = new System.Windows.Forms.TextBox();
+            this.decryptBtn = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.filteredTextVal = new System.Windows.Forms.Label();
+            this.keyA = new System.Windows.Forms.NumericUpDown();
+            this.keyB = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.encryptOutput = new System.Windows.Forms.TextBox();
+            this.decryptOutput = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.keyA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.keyB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // encryptBtn
+            // 
+            this.encryptBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(102)))), ((int)(((byte)(141)))));
+            this.encryptBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.encryptBtn.FlatAppearance.BorderSize = 0;
+            this.encryptBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(102)))), ((int)(((byte)(200)))));
+            this.encryptBtn.Font = new System.Drawing.Font("Yu Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.encryptBtn.ForeColor = System.Drawing.Color.White;
+            this.encryptBtn.Location = new System.Drawing.Point(400, 160);
+            this.encryptBtn.Name = "encryptBtn";
+            this.encryptBtn.Size = new System.Drawing.Size(191, 66);
+            this.encryptBtn.TabIndex = 0;
+            this.encryptBtn.Text = "Encrypt";
+            this.encryptBtn.UseVisualStyleBackColor = false;
+            this.encryptBtn.Click += new System.EventHandler(this.encryptBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic", 11F);
+            this.label1.Location = new System.Drawing.Point(171, 163);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "a:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic", 11F);
+            this.label2.Location = new System.Drawing.Point(171, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "b:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(158, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Text for encryption:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(158, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(182, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Text for decryption:";
+            // 
+            // textBoxEncrypt
+            // 
+            this.textBoxEncrypt.Location = new System.Drawing.Point(302, 51);
+            this.textBoxEncrypt.Name = "textBoxEncrypt";
+            this.textBoxEncrypt.Size = new System.Drawing.Size(614, 34);
+            this.textBoxEncrypt.TabIndex = 5;
+            // 
+            // textBoxDecrypt
+            // 
+            this.textBoxDecrypt.Location = new System.Drawing.Point(302, 90);
+            this.textBoxDecrypt.Name = "textBoxDecrypt";
+            this.textBoxDecrypt.Size = new System.Drawing.Size(614, 34);
+            this.textBoxDecrypt.TabIndex = 6;
+            // 
+            // decryptBtn
+            // 
+            this.decryptBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(102)))), ((int)(((byte)(141)))));
+            this.decryptBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.decryptBtn.FlatAppearance.BorderSize = 0;
+            this.decryptBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(102)))), ((int)(((byte)(200)))));
+            this.decryptBtn.Font = new System.Drawing.Font("Yu Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.decryptBtn.ForeColor = System.Drawing.Color.White;
+            this.decryptBtn.Location = new System.Drawing.Point(611, 160);
+            this.decryptBtn.Name = "decryptBtn";
+            this.decryptBtn.Size = new System.Drawing.Size(191, 66);
+            this.decryptBtn.TabIndex = 9;
+            this.decryptBtn.Text = "Decrypt";
+            this.decryptBtn.UseVisualStyleBackColor = false;
+            this.decryptBtn.Click += new System.EventHandler(this.decryptBtn_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(172, 303);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 22);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Filtered text:";
+            // 
+            // filteredTextVal
+            // 
+            this.filteredTextVal.AutoSize = true;
+            this.filteredTextVal.Location = new System.Drawing.Point(285, 303);
+            this.filteredTextVal.Name = "filteredTextVal";
+            this.filteredTextVal.Size = new System.Drawing.Size(22, 22);
+            this.filteredTextVal.TabIndex = 13;
+            this.filteredTextVal.Text = "...";
+            // 
+            // keyA
+            // 
+            this.keyA.Location = new System.Drawing.Point(213, 160);
+            this.keyA.Name = "keyA";
+            this.keyA.Size = new System.Drawing.Size(46, 34);
+            this.keyA.TabIndex = 14;
+            this.keyA.ValueChanged += new System.EventHandler(this.keyA_ValueChanged);
+            // 
+            // keyB
+            // 
+            this.keyB.Location = new System.Drawing.Point(213, 192);
+            this.keyB.Name = "keyB";
+            this.keyB.Size = new System.Drawing.Size(46, 34);
+            this.keyB.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(172, 360);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(151, 22);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Encrypted output:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(172, 420);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(152, 22);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Decrypted output:";
+            // 
+            // encryptOutput
+            // 
+            this.encryptOutput.Location = new System.Drawing.Point(329, 355);
+            this.encryptOutput.Name = "encryptOutput";
+            this.encryptOutput.Size = new System.Drawing.Size(614, 34);
+            this.encryptOutput.TabIndex = 18;
+            this.encryptOutput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // decryptOutput
+            // 
+            this.decryptOutput.Location = new System.Drawing.Point(329, 413);
+            this.decryptOutput.Name = "decryptOutput";
+            this.decryptOutput.Size = new System.Drawing.Size(614, 34);
+            this.decryptOutput.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(248)))), ((int)(((byte)(240)))));
+            this.ClientSize = new System.Drawing.Size(1067, 588);
+            this.Controls.Add(this.decryptOutput);
+            this.Controls.Add(this.encryptOutput);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.keyB);
+            this.Controls.Add(this.keyA);
+            this.Controls.Add(this.filteredTextVal);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.decryptBtn);
+            this.Controls.Add(this.textBoxDecrypt);
+            this.Controls.Add(this.textBoxEncrypt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.encryptBtn);
+            this.Font = new System.Drawing.Font("Yu Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Affiny cipher encryption/decryprion tool";
+            ((System.ComponentModel.ISupportInitialize)(this.keyA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.keyB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button encryptBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxEncrypt;
+        private System.Windows.Forms.TextBox textBoxDecrypt;
+        private System.Windows.Forms.Button decryptBtn;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label filteredTextVal;
+        private System.Windows.Forms.NumericUpDown keyA;
+        private System.Windows.Forms.NumericUpDown keyB;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox encryptOutput;
+        private System.Windows.Forms.TextBox decryptOutput;
+    }
+}
+
---
+++ Affine/Form1.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Affine
+{
+    public partial class Form1 : Form
+    {
+        private char[] engAlphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+        private int keyAValidation(int a, int b = 26)
+        {
+            int remainder = -1;
+
+            if (a != 0 && b != 0)
+            {
+                a = (a < 0) ? Math.Abs(a) : a;
+                b = (b < 0) ? Math.Abs(b) : b;
+
+                int tmp;
+                int divider;
+
+                if (a > b)
+                {
+                    tmp = a;
+                    a = b;
+                    b = tmp;
+                }
+
+                while (remainder != 0)
+                {
+                    divider = b / a;
+                    remainder = b - divider * a;
+
+                    if (remainder == 0)
+                    {
+                        return a;
+                    }
+
+                    b = a;
+                    a = remainder;
+                }
+            }
+
+            return a;
+        }
+
+        private int getIndexPosition(char letter)
+        {
+            if (engAlphabet.Contains(letter))
+            {
+                return Array.IndexOf(engAlphabet, letter);
+            }
+            else if(letter == ' ')
+            {
+                return -2;
+            }
+
+            return -1;
+        }
+
+        private int changeIndexes(int index, sbyte parA, sbyte parB)
+        {
+            if (index >= 0)
+            {
+                int changedIndex = parA * index + parB % engAlphabet.Length;
+                while (changedIndex > (engAlphabet.Length - 1))
+                {
+                    changedIndex -= engAlphabet.Length;
+                }
+                return changedIndex;
+            }
+
+            else if (index == -2)
+            {
+                return -2;
+            }
+
+            return -1;
+        }
+
+
+        private void encryption(string openText, sbyte parameterA, sbyte parameterB)
+        {
+            char[] specialCharacters = { '.', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', 
+                                         '+', '-', '}', '{', '?', '>', '<', ':', '|', '[', ']', 
+                                       };
+
+            openText = openText.Trim(specialCharacters).TrimEnd();
+
+            string normalizedString = openText.Normalize(NormalizationForm.FormD);
+            int[] indexPositions = new int[openText.Length];
+
+            StringBuilder engText = new StringBuilder();
+
+            for (int i = 0; i < normalizedString.Length; i++)
+            {
+                UnicodeCategory uc = CharUnicodeInfo.GetUnicodeCategory(normalizedString[i]);
+                if (uc != UnicodeCategory.NonSpacingMark)
+                {
+                    engText.Append(normalizedString[i]);
+                }
+            }
+            
+            //eng output containing spaces
+            string engOutput = engText.ToString().ToUpper();
+            filteredTextVal.Text = engOutput;
+
+            for (int i=0; i < engOutput.Length; i++)
+            {
+                indexPositions[i] = getIndexPosition(engOutput[i]);
+            }
+
+            int[] changedIndexes = new int[engOutput.Length];
+
+            for(int i=0; i < engOutput.Length; i++)
+            {
+                changedIndexes[i] = changeIndexes(indexPositions[i], parameterA, parameterB);
+            }
+
+            StringBuilder encryptedLetters = new StringBuilder();
+            int j = 0;
+            foreach (int index in changedIndexes)
+            {
+                //encryptedLetters = (index == 2) ? encryptedLetters.Append("XQW") : encryptedLetters.Append(engAlphabet[index].ToString());
+
+                if (index == -2)
+                {
+                    encryptedLetters.Append("XQW");
+                }
+                else
+                {
+                    encryptedLetters.Append(engAlphabet[index].ToString());
+                }
+                j++;
+            }
+
+            encryptOutput.Text = "";
+
+            for (int i = 0; i < encryptedLetters.Length; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    encryptOutput.Text += " ";
+                }
+                encryptOutput.Text += encryptedLetters[i].ToString();
+            }
+
+        }
+
+        private int modInverse(int a, int sizeOfAlphabet = 26)
+        {
+            a = a % sizeOfAlphabet;
+            
+            for (int i = 1; i < sizeOfAlphabet; i++)
+                if ((a * i) % sizeOfAlphabet == 1)
+                {
+                    return i;
+                }
+            
+            return 0;
+        }
+
+        private int decryptIndex(int index, int invertedA, int b)
+        {
+            int decryptedIndex = ((index - b) * invertedA) % engAlphabet.Length;
+            return (decryptedIndex < 0) ? decryptedIndex + engAlphabet.Length : decryptedIndex;
+        }
+
+        private void decryption(string encryptedText, sbyte keyA, sbyte keyB)
+        {
+            encryptedText = encryptedText.Replace(" ", "");
+            encryptedText = (encryptedText.Contains("XQW")) ? encryptedText.Replace("XQW", " ") : encryptedText;
+
+            int[] encryptedTextIndexes = new int[encryptedText.Length];
+
+            for(int i = 0; i < encryptedText.Length; i++)
+            {
+                encryptedTextIndexes[i] =  getIndexPosition(encryptedText[i]);
+            }
+
+            int[] decryptedIndexes = new int[encryptedTextIndexes.Length];
+            int multInversion = modInverse(keyA);
+
+            for (int i = 0; i < encryptedTextIndexes.Length; i++)
+            {
+                decryptedIndexes[i] = (encryptedTextIndexes[i] == -2) ? decryptedIndexes[i] = -2 : decryptedIndexes[i] = decryptIndex(encryptedTextIndexes[i], multInversion, keyB); 
+            }
+
+            StringBuilder decryptedText = new StringBuilder();
+
+            int c = 0;
+
+            foreach (var index in decryptedIndexes)
+            {
+                if (decryptedIndexes[c] == -2)
+                {
+                    decryptedText.Append(" ");
+                }
+                else
+                {
+                    decryptedText.Append(engAlphabet[index]);
+                }
+                c++;
+            }
+
+            decryptOutput.Text = decryptedText.ToString();
+        }
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void encryptBtn_Click(object sender, EventArgs e)
+        {
+            string openText = textBoxEncrypt.Text;
+            if (openText.Length < 1)
+            {
+                MessageBox.Show("If you want to encrypt text, you should enter it.");
+            }
+            sbyte parA = -1;
+            sbyte parB = -1;
+            try
+            {
+                parA = Convert.ToSByte(keyA.Text);
+                parB = Convert.ToSByte(keyB.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+
+            if (keyAValidation(parA) != 1)
+            {
+                MessageBox.Show("Key parameter a is not matching: gcd(a,26) = 1\nEnter another parameter (a)\nHint: odd numbers");
+            }
+            else
+            {
+                encryption(openText, parA, parB);
+            }
+        }
+
+        private void decryptBtn_Click(object sender, EventArgs e)
+        {
+            string encryptedText = textBoxDecrypt.Text;
+            if (encryptedText.Length < 1)
+            {
+                MessageBox.Show("If you want to decrypt text, you should enter it.");
+            }
+            else
+            {
+                sbyte parA = -1;
+                sbyte parB = -1;
+
+                try
+                {
+                    parA = Convert.ToSByte(keyA.Text);
+                    parB = Convert.ToSByte(keyB.Text);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                if (keyAValidation(parA) != 1)
+                {
+                    MessageBox.Show("Key parameter a is not matching: gcd(a,26) = 1\nEnter another parameter (a)\nHint: odd numbers");
+                }
+                else
+                {
+                    decryption(encryptedText, parA, parB);
+                }
+            }
+
+            
+            
+        }
+
+        private void keyA_ValueChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void decryptOutput_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
